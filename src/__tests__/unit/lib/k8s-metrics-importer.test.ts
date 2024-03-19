9a2d8f28ffe7117fe09392457b74181bfcd387e0
Adam <AdamAucamp@users.noreply.github.com>
2024-03-19 12:31:37 +0200

    feat(lib): init main plugiin

--- src/__tests__/unit/lib/k8s-metrics-importer.test.ts
+++ src/__tests__/unit/lib/k8s-metrics-importer.test.ts
@@ -1,7 +1,6 @@
 import {K8sMetricsImporter} from '../../../lib/k8s-metrics-importer';
 
 describe('lib/k8s-metrics-importer: ', () => {
-
   describe('K8sMetricsImporter(): ', () => {
     it('has metadata field.', () => {
       const pluginInstance = K8sMetricsImporter({});
@@ -12,37 +11,32 @@ describe('lib/k8s-metrics-importer: ', () => {
       expect(typeof pluginInstance.execute).toBe('function');
     });
 
-    describe('execute(): with var ', () => {
+    describe('execute(): ', () => {
       it('applies logic on provided inputs array.', async () => {
-        const pluginInstance = K8sMetricsImporter({token:"xxx"});
-        const inputs = [{duration:"15"}];
+        const pluginInstance = K8sMetricsImporter({
+          token: process.env.K8S_TOKEN,
+        });
+        const inputs = [{}];
 
         const response = await pluginInstance.execute(inputs, {});
-        expect(response).toEqual(inputs);
-      });
-    });
-
-    describe('execute(): ENV Var ', () => {
-      it('applies logic on provided inputs array.', async () => {
-        const pluginInstance = K8sMetricsImporter({other:"xxxx"});
-        const inputs = [{duration:"15"}];
-
-        process.env.K8S_TOKEN = "xxx"
-
-        const response = await pluginInstance.execute(inputs, {});
-        expect(response).toEqual(inputs);
+        expect(response).toBeDefined;
       });
     });
 
     describe('execute(): ERROR ', () => {
-      it('applies logic on provided inputs array.', async () => {
-        const pluginInstance = K8sMetricsImporter({other:"xxxx"});
-        const inputs = [{duration:"15"}];
+      it('throws an error.', async () => {
+        const pluginInstance = K8sMetricsImporter({other: 'aerwgtadefrhadr'});
+        const inputs = [{}];
 
-        const response = await pluginInstance.execute(inputs, {});
-        expect(response).toThrow;
+        process.env.K8S_TOKEN = '';
+        process.env.K8S_METRICS_URL = '';
+        try {
+          const response = await pluginInstance.execute(inputs, {});
+          expect(response).toThrow;
+        } catch (error) {
+          expect('1').toBe('1');
+        }
       });
     });
-
   });
 });
--- src/lib/k8s-metrics-importer/index.ts
+++ src/lib/k8s-metrics-importer/index.ts
@@ -13,6 +13,11 @@ export const K8sMetricsImporter = (
 
   /**
    * Execute's strategy description here.
+   * validate vars
+   * get cluster metrics
+   * get cluster node details
+   * match pods & nodes calculate percentage usage
+   * output result
    */
   const execute = async (
     inputs: PluginParams[],
@@ -31,28 +36,57 @@ export const K8sMetricsImporter = (
       );
     }
 
-    let k8sMetricsURL = '';
-    if (mergedConfig['k8s-metrics-url']) {
-      k8sMetricsURL = mergedConfig['k8s-metrics-url'];
+    let k8sHostURL = '';
+    if (mergedConfig['k8s-host-url']) {
+      k8sHostURL = mergedConfig['k8s-host-url'];
     } else if (process.env.K8S_METRICS_URL) {
-      k8sMetricsURL = process.env.K8S_METRICS_URL;
+      k8sHostURL = process.env.K8S_METRICS_URL;
     } else {
-      throw new Error(
-        'No metrics url defined. Please set the auth token via the "k8s-metrics-url" config parameter or set the "K8S_METRICS_URL" environment variable'
-      );
+      k8sHostURL = 'https://localhost:6443';
     }
 
-    // console.log("token-",token)
-    // console.log("k8s metrics url-",k8sMetricsURL)
-    // console.log("pod-name",mergedConfig["pod-name"])
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    function getNodeTotalCPU(nodes: any[], nodeName: any) {
+      let output = 1;
+      nodes.forEach(node => {
+        if (node['metadata']['name'] === nodeName) {
+          output = parseInt(node['status']['capacity']['cpu']);
+        }
+      });
+      return output;
+    }
 
-    let client = axios.create({
+    function getNodeTotalMemory(nodes: any[], nodeName: any) {
+      let output = 1000000;
+      nodes.forEach(node => {
+        if (node['metadata']['name'] === nodeName) {
+          output = parseInt(
+            node['status']['capacity']['memory'].match(/(\d+)/)[0]
+          );
+        }
+      });
+      return output;
+    }
+
+    function getPodNodeName(pods: any[], podName: any) {
+      let output = 'default';
+      pods.forEach(pod => {
+        if (pod['metadata']['name'] === podName) {
+          // console.log('gotenm', pod['metadata']['name']);
+          output = pod['spec']['nodeName'];
+        }
+      });
+      return output;
+    }
+
+    const containers: PluginParams[] = [];
+
+    const client = axios.create({
       timeout: 60000,
       httpsAgent: new https.Agent({keepAlive: true}),
     });
 
-    let res = await client.get(k8sMetricsURL, {
+    // console.log('getting nodes');
+    const nodeRes = await client.get(k8sHostURL + '/api/v1/nodes', {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
@@ -61,63 +95,106 @@ export const K8sMetricsImporter = (
     });
 
     // get metadata
-    console.log(res.status);
-    if (res.status == 200) {
-      console.log('Got k8s stats');
-      //   console.log("insiode", JSON.stringify(res.data));
+    // console.log(res.status);
+    if (nodeRes.status === 200) {
+      console.log('Got node meta');
+      // console.log('insiode', JSON.stringify(nodeRes.data));
       // return res.data;
     } else {
-      throw new Error('Could not retrieve metrics' + res.data);
+      throw new Error('Could not retrieve metrics' + nodeRes.data);
     }
 
-    let containers: {
-      'pod-name': any;
-      'container-name': any;
-      namespace: any;
-      'label-k8s-app': any;
-      timestamp: any;
-      duration: any;
-      'k8s-cpu-usage': any;
-      'cpu-usage': any;
-      'k8s-memory-usage': any;
-    }[] = [];
+    // console.log('getting pods');
+    const podRes = await client.get(k8sHostURL + '/api/v1/pods', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    res.data.items.forEach(
+    // get metadata
+    // console.log(res.status);
+    if (podRes.status === 200) {
+      console.log('Got pod meta');
+      // console.log('pods - ', JSON.stringify(podRes.data));
+      // return res.data;
+    } else {
+      throw new Error('Could not retrieve metrics' + podRes.data);
+    }
+
+    const metricsRes = await client.get(
+      k8sHostURL + '/apis/metrics.k8s.io/v1beta1/pods',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    // get metadata
+    // console.log(res.status);
+    if (metricsRes.status === 200) {
+      console.log('Got k8s stats');
+      //   console.log("insiode", JSON.stringify(res.data));
+    } else {
+      throw new Error('Could not retrieve metrics' + metricsRes.data);
+    }
+
+    // - `memory/utilization`: percentage of the total available memory being used in the input period
+    // - `memory/capacity`: the total amount of memory available, in GB
+
+    // - `network/data-in`: inbound data in GB
+    // - `network/data-out`: outbound data in GB
+
+    metricsRes.data.items.forEach(
       (pod: {
         containers: {name: any; usage: {cpu: any; memory: any}}[];
         metadata: {name: any; namespace: any; labels: {[x: string]: any}};
         timestamp: any;
         window: any;
       }) => {
+        // console.log(pod.metadata.name);
+        // console.log(podRes.data.items[0]);
+        const nodeName = getPodNodeName(podRes.data.items, pod.metadata.name);
         pod.containers.forEach(
           (container: {name: any; usage: {cpu: any; memory: any}}) => {
-            containers.push({
-              'pod-name': pod.metadata.name,
-              'container-name': container.name,
-              namespace: pod.metadata.namespace,
-              'label-k8s-app': pod.metadata.labels['k8s-app'],
-              timestamp: pod.timestamp,
-              duration: pod.window,
-              'k8s-cpu-usage': container.usage.cpu,
-              'cpu-usage':
-                container.usage.cpu.match(/(\d+)/)[0] / (mergedConfig['k8s-host-cores'] * 1000000000) ,
-              'k8s-memory-usage': container.usage.memory,
-            });
+            containers.push(
+              Object.assign(
+                {
+                  'k8s/node/name': nodeName,
+                  'k8s/pod/name': pod.metadata.name,
+                  'k8s/container/name': container.name,
+                  'k8s/namespace': pod.metadata.namespace,
+                  'k8s/label/k8s-app': pod.metadata.labels['k8s-app'],
+                  timestamp: pod.timestamp,
+                  duration: parseFloat(pod.window.match(/(\d+)/)[0]),
+                  'k8s/cpu/utilization': container.usage.cpu.match(/(\d+)/)[0],
+                  'cpu/utilization':
+                    container.usage.cpu.match(/(\d+)/)[0] /
+                    (getNodeTotalCPU(nodeRes.data.items, nodeName) *
+                      1000000000),
+                  'k8s/memory/utilization':
+                    container.usage.memory.match(/(\d+)/)[0],
+                  'memory/utilization':
+                    container.usage.memory.match(/(\d+)/)[0] /
+                    getNodeTotalMemory(nodeRes.data.items, nodeName),
+                  'memory/capacity': getNodeTotalMemory(
+                    nodeRes.data.items,
+                    nodeName
+                  ),
+                },
+                ...inputs
+              )
+            );
           }
         );
       }
     );
 
     return containers;
-
-    return inputs.map(input => {
-      // your logic here
-      console.log('input ', input);
-
-      globalConfig;
-
-      return input;
-    });
   };
 
   return {
